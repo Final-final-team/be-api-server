de0d4af34332ca5f2c268aa584a8467bd632ebbb
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-11 11:58:50 +0900

    fix(global): 불필요 파일 정리

--- src/main/java/com/example/workmanagement/domain/task/entity/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.workmanagement.domain.task.entity;
-
-public enum TaskStatus {
-    IN_PROGRESS,
-    IN_REVIEW,
-    COMPLETED
-}
-
--- src/main/java/com/example/workmanagement/global/error/NotYetImplementedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.workmanagement.global.error;
-
-public class NotYetImplementedException extends DomainException {
-
-    public NotYetImplementedException(String message) {
-        super(ApiErrorCode.NOT_IMPLEMENTED, message);
-    }
-}
-
