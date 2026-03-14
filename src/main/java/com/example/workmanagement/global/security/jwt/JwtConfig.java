832babf62afb9d9cab64b64de9722d01d910e53d
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-15 07:00:04 +0900

    feat(global): JwtConfig 정의
    - jwtSecretKey 빈 팩토리 메서드 정의
      - HmacSHA256 알고리즘 사용
    - jwtEncoder 빈 팩토리 메서드 정의
    - jwtDecoder 빈 팩토리 메서드 정의
      - 애플리케이션의 액세스 토큰을 검증하는 로직을, 스프링 시큐리티에서 기본 제공하는 validator 를 적극 활용하여 구현함
      - 자세한 내용은 관련 메서드 내에 주석으로 설명해놓았으니 참고 바람

---
+++ src/main/java/com/example/workmanagement/global/security/jwt/JwtConfig.java
@@ -0,0 +1,93 @@
+package com.example.workmanagement.global.security.jwt;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtAudienceValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+
+    private final JwtProperties jwtProperties;
+
+    public JwtConfig(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+    }
+
+    @Bean
+    SecretKey jwtSecretKey() {
+
+        final String SECRET_KEY_ALGORITHM = "HmacSHA256";
+
+        return new SecretKeySpec(
+                jwtProperties.secret().getBytes(StandardCharsets.UTF_8),
+                SECRET_KEY_ALGORITHM
+        );
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(SecretKey jwtSecretKey) {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtSecretKey));
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(
+            SecretKey jwtSecretKey
+    ) {
+
+        // 서명 검증(jwtSecretKey)
+        NimbusJwtDecoder decoder = NimbusJwtDecoder
+                .withSecretKey(jwtSecretKey)
+                .build();
+
+        /*
+         * 디폴트 validator
+         * 디폴트 validator 에서 제공하는 검증이 누락될 위험을 줄이기 위해 명시
+         * 유효시간검증(exp, nbf) 등 포함
+         * 만약 기본 clock skew (60초) 를 변경하고 싶다면 아래와 같이 validator 를 추가해야 함
+         *
+         * ```
+         * OAuth2TokenValidator<Jwt> withTimestamp =
+         * new JwtTimestampValidator(Duration.ofSeconds(60));
+         * ```
+         */
+        OAuth2TokenValidator<Jwt> withDefault
+                = JwtValidators.createDefault();
+        
+        // issuer 검증
+        String issuer = jwtProperties.issuer();
+        OAuth2TokenValidator<Jwt> withIssuer
+                = new JwtIssuerValidator(issuer);
+
+        // audience 검증
+        String audience = jwtProperties.audience();
+        OAuth2TokenValidator<Jwt> withAudience
+                = new JwtAudienceValidator(audience);
+
+        // validator 조합
+        OAuth2TokenValidator<Jwt> validator =
+                new DelegatingOAuth2TokenValidator<>(
+                        withDefault,
+                        withIssuer,
+                        withAudience
+                );
+
+        decoder.setJwtValidator(validator);
+        return decoder;
+    }
+}
