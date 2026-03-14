6f51da1afd13614017480544883a8659d10fd909
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-15 06:56:47 +0900

    feat(user): JPA 리포지토리 인터페이스 정의

---
+++ src/main/java/com/example/workmanagement/domain/user/repository/SocialAccountRepository.java
@@ -0,0 +1,12 @@
+package com.example.workmanagement.domain.user.repository;
+
+import com.example.workmanagement.domain.user.domain.model.SocialAccount;
+import com.example.workmanagement.domain.user.domain.model.enums.AuthProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+
+    Optional<SocialAccount> findByProviderAndSub(AuthProvider provider, String sub);
+}
