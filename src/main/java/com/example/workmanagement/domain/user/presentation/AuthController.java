899926d5bbe67c1a68ba135bd0ecaafa78093336
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-15 07:08:45 +0900

    feat(user): 로그인 테스트 용 임시 컨트롤러 핸들러 정의
    - 확인 결과 토큰 잘 내려가고 있음

---
+++ src/main/java/com/example/workmanagement/domain/user/presentation/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.workmanagement.domain.user.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    /**
+     * 루트 경로에 대한 핸들러
+     */
+    @GetMapping("/")
+    public String rooooot() {
+        return "→ /";
+    }
+
+    /**
+     * 리프레시 토큰이 전달되는 경로
+     */
+    @GetMapping("/api/auth")
+    public String ref() {
+        return "→ /api/auth";
+    }
+
+    /**
+     * 구글 OIDC 로그인 성공 시 리다이렉트될 경로의 핸들러
+     */
+    @GetMapping("/login/success")
+    public String loginSuccess() {
+        return "→ LOGIN SUCCESS!";
+    }
+}
