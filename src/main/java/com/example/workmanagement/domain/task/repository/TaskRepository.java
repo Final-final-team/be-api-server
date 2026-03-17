c66a750500e958ae8c7fba6bc057d55deb49f701
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-17 15:45:03 +0900

    feat(task): Task 엔티티에 대한 JPA 리포지토리 정의

---
+++ src/main/java/com/example/workmanagement/domain/task/repository/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.example.workmanagement.domain.task.repository;
+
+import com.example.workmanagement.domain.task.domain.model.Task;
+import com.example.workmanagement.domain.task.domain.model.TaskStatus;
+import java.util.Collection;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findAllByProjectId(Long projectId, Pageable pageable);
+
+    Page<Task> findAllByProjectIdAndStatusIn(Long projectId, Collection<TaskStatus> statuses, Pageable pageable);
+}
